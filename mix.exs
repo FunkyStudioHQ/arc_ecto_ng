78e52c50db1e4459cb015dbc20a856142df40cef
Andrea Pavoni <andrea.pavoni@gmail.com>
2018-03-27 12:05:56 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -1,17 +1,19 @@
 defmodule Arc.Ecto.Mixfile do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.8.0"
 
   def project do
-    [app: :arc_ecto,
-     version: @version,
-     elixir: "~> 1.0",
-     deps: deps(),
+    [
+      app: :arc_ecto,
+      version: @version,
+      elixir: "~> 1.5",
+      deps: deps(),
 
-    # Hex
-     description: description(),
-     package: package()]
+      # Hex
+      description: description(),
+      package: package()
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,10 +30,12 @@ defmodule Arc.Ecto.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Sean Stavropoulos"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
-     files: ~w(mix.exs README.md lib)]
+    [
+      maintainers: ["Sean Stavropoulos"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
+      files: ~w(mix.exs README.md lib)
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -45,9 +49,9 @@ defmodule Arc.Ecto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:arc,  "~> 0.8.0"},
-      {:ecto, "~> 2.0"},
-      {:mock, "~> 0.1.1", only: :test},
+      {:arc, "~> 0.8.0"},
+      {:ecto, "~> 2.1"},
+      {:mock, "~> 0.3.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
